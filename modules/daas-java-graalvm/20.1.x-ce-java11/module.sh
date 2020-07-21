dab1e8f0fd996292bbcee5c2c5547c67521b1743
David Ward <errantepiphany@users.noreply.github.com>
2020-07-21 16:58:18 -0400

    module renaming (#8)
    Signed-off-by: David Ward <dward@redhat.com>

---
+++ modules/daas-java-graalvm/20.1.x-ce-java11/module.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+install_module() {
+    local artifacts_dir="/tmp/artifacts"
+    tar xzf ${artifacts_dir}/graalvm-ce-java${GRAALVM_JAVA_VERSION}-linux-amd64-${GRAALVM_VERSION}.tar.gz -C /usr/share
+    ln -s /usr/share/graalvm-ce-java${GRAALVM_JAVA_VERSION}-${GRAALVM_VERSION} ${GRAALVM_HOME}
+
+    ${GRAALVM_HOME}/bin/gu -L install ${artifacts_dir}/native-image-installable-svm-java${GRAALVM_JAVA_VERSION}-linux-amd64-${GRAALVM_VERSION}.jar
+
+    # see KOGITO-384
+    # also note path '/lib/' below differs from 20.1.x-ce-java8, which is '/jre/lib/'
+    mkdir -p ${DAAS_HOME}/ssl-libs
+    cp -v ${GRAALVM_HOME}/lib/libsunec.so ${DAAS_HOME}/ssl-libs/
+    cp -v ${GRAALVM_HOME}/lib/security/cacerts ${DAAS_HOME}/
+
+    echo 'export PATH="/usr/share/graalvm/bin:${PATH}"' >> ${DAAS_HOME}/.bashrc
+}
+
+install_module ${@}
