b38f0018e56a9c7157cecfb67b8f07ab08f48af6
David Ward <errantepiphany@users.noreply.github.com>
2020-07-24 19:12:59 -0400

    made httpd run as daas user (#11)
    Signed-off-by: David Ward <dward@redhat.com>

--- modules/daas-www-httpd/module.sh
+++ modules/daas-www-httpd/module.sh
@@ -11,13 +11,25 @@ install_module() {
     mkdir -p /var/www/html
     cp -v -r ${SCRIPT_DIR}/www/* /var/www/html
     mkdir -p /var/www/webdav
-    chown -R apache:apache /var/www
 
     mkdir -p /etc/httpd/conf.d
     cp -v -r ${SCRIPT_DIR}/conf.d/* /etc/httpd/conf.d
 
-    sed -i "s/Listen 80/Listen 8080/g" "/etc/httpd/conf/httpd.conf"
-    sed -i "s/#ServerName www.example.com:80/ServerName localhost:8080/g" "/etc/httpd/conf/httpd.conf"
+    for http_dir in /etc/httpd /var/www /var/log/httpd /var/run/httpd /run/httpd ; do
+        chown -R ${USER}:${USER} ${http_dir}
+        chmod -R 775 ${http_dir}
+    done
+
+    # usermod -aG apache ${USER}
+
+    local http_host="${HTTP_HOST:-localhost}"
+    local http_port="${HTTP_PORT:-8080}"
+    local http_conf="/etc/httpd/conf/httpd.conf"
+
+    sed -i "s/Listen 80/Listen ${http_port}/g" "${http_conf}"
+    sed -i "s/#User apache/User ${USER}/g" "${http_conf}"
+    sed -i "s/#Group apache/Group ${USER}/g" "${http_conf}"
+    sed -i "s/#ServerName www.example.com:80/ServerName ${http_host}:${http_port}/g" "${http_conf}"
 }
 
 install_module ${@}
